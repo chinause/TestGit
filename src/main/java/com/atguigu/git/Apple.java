84497cbfaa5e75d3acac4d4b8083703b0eff6c43
eclipse_user <eclipse_user@atguigu.com>
2019-05-07 15:53:59 +0800

    My first commit

---
+++ src/main/java/com/atguigu/git/Apple.java
@@ -0,0 +1,25 @@
+/*
+ * 文件名：Apple.java
+ * 版权：Copyright by www.amarsoft.com
+ * 描述：
+ * 修改人：who
+ * 修改时间：2019年5月6日
+ * 跟踪单号：
+ * 修改单号：
+ * 修改内容：
+ */
+
+package com.atguigu.git;
+
+/**
+ * 〈一句话功能简述〉
+ * 〈功能详细描述〉
+ * @author who
+ * @version 2019年5月6日
+ * @see Apple
+ * @since
+ */
+
+public class Apple {
+
+}
