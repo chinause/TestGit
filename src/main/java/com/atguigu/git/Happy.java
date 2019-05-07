7351d5b485d0e62683fcc1483c552b4f5cd50a24
eclipse_user <eclipse_user@atguigu.com>
2019-05-07 15:55:06 +0800

    Second commit

---
+++ src/main/java/com/atguigu/git/Happy.java
@@ -0,0 +1,25 @@
+/*
+ * 文件名：Happy.java
+ * 版权：Copyright by www.amarsoft.com
+ * 描述：
+ * 修改人：who
+ * 修改时间：2019年5月7日
+ * 跟踪单号：
+ * 修改单号：
+ * 修改内容：
+ */
+
+package com.atguigu.git;
+
+/**
+ * 〈一句话功能简述〉
+ * 〈功能详细描述〉
+ * @author who
+ * @version 2019年5月7日
+ * @see Happy
+ * @since
+ */
+
+public class Happy {
+
+}
